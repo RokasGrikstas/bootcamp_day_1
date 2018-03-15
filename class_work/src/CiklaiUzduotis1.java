7d0c8467549d0786dcec11d18b865e81863e8391
RokasGrikstas <grikstas1992@gmail.com>
2018-03-15 11:53:34 +0200

    sekantis komentaras

--- class_work/src/CiklaiUzduotis1.java
+++ class_work/src/CiklaiUzduotis1.java
@@ -2,10 +2,14 @@ public class CiklaiUzduotis1 {
 
     public static void main(String[] args) {
 
-        for(int i =0; i < 5; i++){
+        for (int i = 0; i < 5; i++) {
 
             System.out.println("Hello world");
 
         }
+
+
+        private static void stest(int size, scanner sc)
     }
+
 }
